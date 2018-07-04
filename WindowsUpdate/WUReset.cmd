784a18b5eff6a7b75e76325548a0af37e2a82465
Ermanno Goletto <e.goletto@outlook.it>
2018-07-04 09:42:09 +0200

    Update WUReset.cmd

--- WindowsUpdate/WUReset.cmd
+++ WindowsUpdate/WUReset.cmd
@@ -1,11 +1,31 @@
-net stop wuauserv
-net stop bits
- 
-rmdir /S /Q %windir%\SoftwareDistribution
+@ECHO OFF
 
-net start wuauserv
-net start bits
+ECHO *** Stop service wuauserv ***
+NET STOP wuauserv
 
+ECHO *** Kill process service wuauserv ***
+FOR /F "tokens=3" %%A IN ('SC QUERYEX wuauserv ^| FINDSTR PID') DO (SET pid=%%A)
+IF "%pid%" NEQ "0" (
+  taskkill /F /PID %pid%
+)
+
+ECHO *** Stop service bits ***
+NET STOP bits
+
+ECHO *** Kill process service bits ***
+FOR /F "tokens=3" %%A IN ('sc queryex bits ^| FINDSTR PID') DO (SET pid=%%A)
+IF "%pid%" NEQ "0" (
+  ECHO taskkill /F /PID %pid%
+)
+
+ECHO *** Delete directory %windir%\SoftwareDistribution ***
+RMDIR /S /Q %windir%\SoftwareDistribution
+
+ECHO *** Start services wuauserv and bits ***
+NET START wuauserv
+NET START bits
+
+ECHO *** Start search updates ***
 wuauclt.exe /resetauthorization /detectnow
 
-pause
+PAUSE
