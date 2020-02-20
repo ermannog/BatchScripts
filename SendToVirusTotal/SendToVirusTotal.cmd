c250bf645e42766612d5f82e0c82e111a9f20ac8
Ermanno Goletto <e.goletto@outlook.it>
2020-02-20 15:00:13 +0100

    Update SendToVirusTotal.cmd

--- SendToVirusTotal/SendToVirusTotal.cmd
+++ SendToVirusTotal/SendToVirusTotal.cmd
@@ -1,6 +1,15 @@
 @echo off
+IF NOT EXIST %appdata%\Microsoft\Windows\SendTo\VirusTotal.lnk GOTO CreateSendToLink
+IF "%~1" == "" GOTO End
+
 SET OUTPUTFILE=%~dp0sigcheckout.txt
 %~dp0sigcheck64 -vt -vs %1 > %OUTPUTFILE%
 findstr /i /c:"VT link:" %OUTPUTFILE%
 for /f "tokens=3" %%a in ('findstr /i /c:"VT link:" "%OUTPUTFILE%"') do set VTLink=%%a
 "%ProgramFiles%\Internet Explorer\iexplore.exe" "%VTLink%"
+GOTO End
+
+:CreateSendToLink
+%~dp0CreateSendToShortcut.vbs "VirusTotal" %0 %~dp0VirusTotal.ico %~dp0
+
+:End
