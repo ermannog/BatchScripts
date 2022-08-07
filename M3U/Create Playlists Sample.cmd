1db59da424227e38a5238e8058950f1f40ebae1e
Ermanno Goletto <e.goletto@outlook.it>
2022-08-07 23:08:06 +0200

    Update Create Playlists Sample.cmd

--- M3U/Create Playlists Sample.cmd
+++ M3U/Create Playlists Sample.cmd
@@ -2,18 +2,12 @@
 setlocal enabledelayedexpansion
 
 ECHO Generazione Playlist per subdirectory Pop Italiano
-del /q "Pop Italiano.m3u"
-dir /b /on /s ".\Pop Italiano\*.mp3">dir.tmp
-call "Create m3u.cmd" dir.tmp "Pop Italiano.m3u"
+call "Create m3u from directory.cmd" ".\Pop Italiano\*.mp3" "Pop Italiano.m3u"
 
 ECHO Generazione Playlist per subdirectory Rock
-del /q "Rock.m3u"
-dir /b /on /s ".\Rock\*.mp3">dir.tmp
-call "Create m3u.cmd" dir.tmp "Rock.m3u"
+call "Create m3u from directory.cmd" ".\Rock\*.mp3" "Rock.m3u"
 
 ECHO Generazione Playlist Pop Italiano e Rock
 type "Pop Italiano.m3u" "Rock.m3u" > "Pop Italiano e Rock.m3u"
 
-ECHO Eliminazioe file temporanei
-del /q dir.tmp
 pause
