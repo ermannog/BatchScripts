8a67fe9d821a91c103972c58bef9d433b3487eb9
Ermanno Goletto <e.goletto@outlook.it>
2023-04-26 11:25:18 +0200

    Add files via upload

--- Operating System/LogUserSession.cmd
+++ Operating System/LogUserSession.cmd
@@ -1,9 +1,13 @@
 :: Purpose:  Creates log files named computername.username containing the history of user sessions
 :: Author:   Ermanno Goletto
-:: Revision: 3.0 
+:: Revision: 5.0 
 
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM *** Impostazioni Costanti
+REM Impostazione dimensione massima file di log in bytes (0=Nessuna limitazione)
+SET LOGMAXSIZE=76800
 
 REM *** Impostazioni varabili 
 SET MONTH=%DATE:~3,2%
@@ -22,10 +26,6 @@ REM Creazione SubDirectory Logs
 SET LogsSubFolder="%~dp0Logs"
 IF NOT EXIST %LogsSubFolder% MKDIR %LogsSubFolder%
 
-REM Ricerca IP
-FOR /F "TOKENS=4 DELIMS= " %%i in ('ROUTE PRINT ^| FIND " 0.0.0.0"') DO (SET IP=%%i & GOTO :NEXT)
-:NEXT
-
 REM Ricerca sessioni utente
 FOR /F "SKIP=1 TOKENS=1,2,6,7 DELIMS=> " %%a IN ('QUERY USER') DO (
   SET User=%%a
@@ -34,11 +34,17 @@ FOR /F "SKIP=1 TOKENS=1,2,6,7 DELIMS=> " %%a IN ('QUERY USER') DO (
   SET LogonHour=%%d
 
   REM Creazione testo Log
-  SET LogText=%YEAR%-%MONTH%-%DAY% %HOUR%:%MINUTE% Session: !Session! - Logon: !LogonDay! !LogonHour! - IP: %IP%
+  SET LogText=%YEAR%-%MONTH%-%DAY% %HOUR%:%MINUTE% Session: !Session! - Logon: !LogonDay! !LogonHour! - IP:
 
+  REM Ricerca indirizzi IP
+  FOR /F "TOKENS=2 DELIMS=:" %%a IN ('IPCONFIG ^| FINDSTR IPv4') DO (SET LogText=!LOGTEXT!%%a)
+  
   REM Creazione file Computer.Utente ordinato per data di log decrescente
   SET ComputerUserLogFile=%LogsSubFolder%\%COMPUTERNAME%.!User!
   ECHO !LogText!>>!ComputerUserLogFile!.tmp
   TYPE !ComputerUserLogFile!>>!ComputerUserLogFile!.tmp
   MOVE /Y !ComputerUserLogFile!.tmp !ComputerUserLogFile!
+
+  REM Impostazione max size del file di log
+  IF %LOGMAXSIZE% NEQ 0 FSUTIL file seteof !ComputerUserLogFile! %LOGMAXSIZE%
 )
