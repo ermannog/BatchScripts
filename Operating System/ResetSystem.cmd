e335c6ca20efd2ce0bf28f86a5058b10331359fe
Ermanno Goletto <e.goletto@outlook.it>
2024-03-22 17:49:03 +0100

    Update ResetSystem.cmd

--- Operating System/ResetSystem.cmd
+++ Operating System/ResetSystem.cmd
@@ -1,6 +1,16 @@
-REM Reset WMI Repository
+@ECHO OFF
+
+ECHO Reset WMI Repository
 NET STOP winmgmt /y
 winmgmt /resetrepository
+
+ECHO Rebuild the performance counters
+lodctr /r
+ECHO You can check performance counters status after rebuild by running the following command:
+ECHO lodctr /q or lodctr /q:[counter name]
+ECHO If you see (Disabled) next to a counter, you can enable it by running the following command:
+ECHO lodctr /e:[counter name]
+ECHO lodctr /r
 
 REM Reset IP
 NETSH int ip reset 
