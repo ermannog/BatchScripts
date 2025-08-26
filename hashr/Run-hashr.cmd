385083520aa10eaea78b7258d63c40b1774dbbd6
Ermanno Goletto <e.goletto@outlook.it>
2025-08-27 00:40:33 +0200

    Add files via upload

--- hashr/Run-hashr.cmd
+++ hashr/Run-hashr.cmd
@@ -1,7 +1,8 @@
-:: Purpose:  Verifica IOC tramite il tool hashr rilasciato dal CERT-AGID
-:: Author:   Ermanno Goletto
-:: Notes:    Il tool hashr deve essere presente nella stessa cartella da cui viene avviato lo script batch
-:: Revision: 1.0
+﻿:: Purpose:		Verifica IOC tramite il tool hashr rilasciato dal CERT-AGID
+:: Author:		Ermanno Goletto
+:: Requirements:	I file contenenti gli hash degli IoC devono essere presenti nella subdirectory IoC della cartella da cui viene avviato lo script batch
+:: Requirements:	Il tool hashr deve essere presente nella cartella da cui viene avviato lo script batch
+:: Revision:		1.2
 
 @echo off
 setlocal enabledelayedexpansion
@@ -9,33 +10,32 @@ setlocal enabledelayedexpansion
 REM *** Impostazioni Costanti
 SET RootFolder=%SystemDrive%\
 SET IoCAbsent=IoCAbsent
+SET PathFileLog=%~dp0%~n0.log
 
 REM *** Impostazioni varabili 
 SET MONTH=%DATE:~3,2%
 SET DAY=%DATE:~0,2%
 SET YEAR=%DATE:~6,4%
 
-SET HOUR=%TIME:~0,2%
-REM sostituzione spazi con 0
-SET HOUR=%HOUR: =0%
 
-SET MINUTE=%TIME:~3,2%
-REM sostituzione spazi con 0
-SET MINUTE=%MINUTE: =0%
+REM *** Clear Log
+ECHO.>"%PathFileLog%"
+
+CALL :Log "Avvio Analisi presenza IoC in %RootFolder%."
+
 
 REM *** Controlla se la cartella IOC esiste.
 SET IoCSubFolder=%~dp0IoC
 IF NOT EXIST "%IoCSubFolder%" (
-  ECHO La cartella %IoCSubFolder% non è stata trovata.
+  CALL :Log "La cartella %IoCSubFolder% non è stata trovata."
   GOTO :eof
 )
-
 
 REM *** Creazione cartella Out.
 SET OutSubFolder=%~dp0Out
 IF NOT EXIST "%OutSubFolder%" MKDIR "%OutSubFolder%"
 IF NOT ERRORLEVEL 0 (
-  ECHO Errore durante la creazione della cartella %OutSubFolder%.
+  CALL :Log "Errore durante la creazione della cartella %OutSubFolder%."
   GOTO :eof
 )
 
@@ -43,46 +43,68 @@ REM *** Creazione cartella LOG.
 SET LogSubFolder=%~dp0Log
 IF NOT EXIST "%LogSubFolder%" MKDIR "%LogSubFolder%"
 IF NOT ERRORLEVEL 0 (
-  ECHO Errore durante la creazione della cartella %LogSubFolder%.
+  CALL :Log "Errore durante la creazione della cartella %LogSubFolder%."
   GOTO :eof
 )
 
-ECHO Avvio dell'analisi dei file nella cartella %IoCSubFolder%...
-ECHO.
+CALL :Log "Ricerca file IoC nella cartella %IoCSubFolder% ..."
 
 FOR %%f IN ("%IoCSubFolder%\*.*") DO (
-    ECHO Esecuzione di hashr.exe per il file: %%f
+
+  CALL :Log "Trovato file IoC %%f."
     
-    REM *** Ottiene il nome del file senza estensione.
-    SET IoCFileName=%%~nf
+  REM *** Ottiene il nome del file senza estensione.
+  SET IoCFileName=%%~nf
 
-    SET OutFile=%OutSubFolder%\!IoCFileName!.%COMPUTERNAME%
+  SET OutFile=%OutSubFolder%\!IoCFileName!.%COMPUTERNAME%
 
-    IF EXIST "!OutFile!.%IoCAbsent%" (
+  IF EXIST "!OutFile!.%IoCAbsent%.*" (
 
-      ECHO L'IoC %%f e' gia' stato verificato.
+    CALL :Log "L'IoC %%f e' gia' stato verificato."
 
-    ) ELSE (
+  ) ELSE (
   
-      REM Esegui il comando hashr.exe.
-      START /BELOWNORMAL /WAIT /B hashr.exe --hashlist "%%f" --rootdir %RootFolder% --output "!OutFile!" --log "%LogSubFolder%\!IoCFileName!.log"
+    REM Esegui il comando hashr.exe.
+    START /BELOWNORMAL /B /WAIT %~dp0hashr.exe --hashlist "%%f" --rootdir %RootFolder% --output "!OutFile!" --log "%LogSubFolder%\!IoCFileName!.%COMPUTERNAME%.log"
+
+    REM *** Controlla che non si siano verificati errori.
+    IF !ERRORLEVEL! NEQ 0 (
+      CALL :Log "Si sono verificati errori durante l'analisi dell'IoC %%f."
+      TYPE NUL > !OutFile!.IoCError.%YEAR%-%MONTH%-%DAY%
+    ) ELSE (
 
       REM *** Controlla la dimensione del file di output.
       FOR %%s in ("!OutFile!") DO SET "OutFileSize=%%~zs"
 
       REM *** Ridenominazioe file di output.
       IF !OutFileSize! EQU 0 (
-         Echo Il file !OutFile! e' vuoto, non sono stati rivelati IoC! Ridenominazione del file.
-         MOVE /Y "!OutFile!" "!OutFile!.%IoCAbsent%"
+        CALL :Log "Il file !OutFile! e' vuoto, non sono stati rivelati IoC."
+        MOVE /Y "!OutFile!" "!OutFile!.%IoCAbsent%.%YEAR%-%MONTH%-%DAY%"
       ) ELSE (
-         Echo Il file !OutFile! non e' vuoto, sono stati rivelati IoC! Ridenominazione del file.
-         MOVE /Y "!OutFile!" "!OutFile!.IoCFound.%YEAR%%MONTH%%DAY%%HOUR%%MINUTE%.txt"
-         MSG * /time:0 "Attenzione Rilevati IoC '!IoCFileName!', avvertire gli amministratori di sistema."
+        CALL :Log "Il file !OutFile! non e' vuoto, sono stati rivelati IoC."
+        MOVE /Y "!OutFile!" "!OutFile!.IoCFound.%YEAR%-%MONTH%-%DAY%.txt"
+        MSG * /time:0 "Attenzione Rilevati IoC '!IoCFileName!', avvertire gli amministratori di sistema."
       )
 
     )
 
-    ECHO.
+  )
+
 )
 
-ECHO Analisi completata.
+CALL :Log "Analisi completata."
+EXIT /b
+
+:Log
+SET "yyyy=%DATE:~6,4%"
+SET "MM=%DATE:~3,2%"
+SET "dd=%DATE:~0,2%"
+SET "hh=%TIME:~0,2%"
+SET "hh=!hh: =0!"
+SET "mm=%TIME:~3,2%"
+SET "mm=!mm: =0!"
+SET "ss=%TIME:~6,2%"
+SET "ss=!ss: =0!"
+ECHO %~1
+>>"%PathFileLog%" ECHO [!yyyy!-!MM!-!dd! !hh!:!!mm!:!ss!] %~1
+EXIT /b
